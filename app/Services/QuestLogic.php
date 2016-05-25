a6bc83bf51d3f0b41da7c69c26dde5b1e4b014a7
Yamashita Sho <yamashita.sho@techcross.co.jp>
2016-05-25 02:16:47 +0000

    意味のない処理があったのを削除、関数構造の整理

--- app/Services/QuestLogic.php
+++ app/Services/QuestLogic.php
@@ -38,28 +38,22 @@ class QuestLogic extends Model
     public function joinQuest()
     {
         $user = $this->userinfo->getUser();
-        if ($user['battle_id'] == 0){
-            return true;                           #バトルIDが0の場合は再利用しない
-        }
 
-        if (!$this->canMakeBattle($user)){       #バトル作成可能条件の確認
-            $response = [
+        if ($this->canMakeBattle($user)){       #バトル作成可能条件の確認
+            #バトルIDを一つ進める
+            $user['battle_id'] ++;
+            #仮enemyparty_id()
+            $enemyparty_id = 0;
+            $enemyparty = $this->quest->readEnemyParty($enemyparty_id);
+            $enemy_position = $this->quest->readEnemy($enemyparty['party']);
+            $friend_position = $this->quest->readCharInParty($user);
+            #ユーザーデータの書き込み(トランザクションに要変更)
+            $this->quest->writeBattle($user, $friend_position, $enemy_position);
+            $this->quest->writeUser($user);
+        }
+        $response = [
                 'battle_id' => $user['battle_id']
-            ];
-            return [$response, 201];               #created、in_processのものが存在する
-        }
-        #バトルIDを一つ進める
-        $user['battle_id'] ++;
-        #仮enemyparty_id()
-        $enemyparty_id = 0;
-        $enemyparty = $this->quest->readEnemyParty($enemyparty_id);
-        $enemy_position = $this->quest->readEnemy($enemyparty['party']);
-        $friend_position = $this->quest->readCharInParty($user);
-        #ユーザーデータの書き込み(トランザクションに要変更)
-        $this->quest->writeBattle($user, $friend_position, $enemy_position);
-        $this->quest->writeUser($user);
-
-        $response = [ "battle_id" => $user['battle_id']];
+        ];
         return [$response, 201];
     }
 
@@ -70,13 +64,13 @@ class QuestLogic extends Model
      */
     private function canMakeBattle($user)
     {
-        if ($user['battle_id'] == 0){
-            return true;                           #バトルIDが0の場合は再利用しない
+        $res = true;
+        if ($user['battle_id'] != 0){
+            $battle = $this->quest->readBattle($user);
+            if ($battle['progress'] != 'closed'){
+                $res = false;
+            }
         }
-        $battle = $this->quest->readBattle($user);
-        if ($battle['progress'] != 'closed'){
-            return false;
-        }
-        return true;
+        return $res;
     }
 }
